213c9bd4b497942aab2222d479e42039e14124c8
Nikolina Kokor <nkokor@icloud.com>
2023-08-10 17:03:43 +0200

    candy switch implemented

--- script.js
+++ script.js
@@ -22,6 +22,77 @@ function setUpBoard() {
   }
 }
 
+let draggedCandy 
+let replacedCandy
+let draggedSquare
+let replacedSquare
+let validMoves
+
+function addEventListeners() {
+  squares.forEach(square => {
+    square.addEventListener('dragstart', dragStart)
+    square.addEventListener('dragover', dragOver)
+    square.addEventListener('dragenter', dragEnter)
+    square.addEventListener('dragleave', dragLeave)
+    square.addEventListener('drop', dragDrop)
+    square.addEventListener('dragend', dragEnd)
+  })
+}
+
+function dragStart() {
+  draggedCandy = this.style.backgroundColor
+  draggedSquare = parseInt(this.id)
+  validMoves = [draggedSquare - 1, draggedSquare + 1, draggedSquare - width, draggedSquare + width]
+  console.log(this.id, 'dragstart')
+}
+
+function dragOver(event) {
+  event.preventDefault()
+  console.log(this.id, 'dragover')
+}
+
+function dragEnter() {
+  console.log(this.id, 'dragenter')
+}
+
+function dragLeave() {
+  console.log(this.id, 'dragleave')
+}
+
+function dragDrop() {
+  replacedCandy = this.style.backgroundColor
+  replacedSquare = parseInt(this.id)
+  if(validMove()) {
+    switchCandies()
+  }
+  console.log(this.id, 'dragdrop')
+}
+
+function dragEnd() {
+  console.log(this.id, 'dragend')
+  if(replacedSquare && validMove()) {
+    replacedSquare = null
+    replacedCandy = null
+    validMoves = []
+    draggedSquare = null
+    draggedCandy = null
+  } else if(replacedSquare && validMove() == false){
+    squares[draggedSquare].style.backgroundColor = draggedCandy
+    squares[replacedSquare].style.backgroundColor = replacedCandy
+  } else {
+    squares[draggedSquare].style.backgroundColor = draggedCandy
+  }
+}
+
+function validMove() {
+  return validMoves.includes(replacedSquare)
+}
+
+function switchCandies() {
+  squares[draggedSquare].style.backgroundColor = replacedCandy
+  squares[replacedSquare].style.backgroundColor = draggedCandy
+}
+
 document.addEventListener('DOMContentLoaded', () => {
   const board = document.getElementById('board')
   setUpBoard()
